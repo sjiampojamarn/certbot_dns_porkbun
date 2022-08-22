a5eab2e41435415b01f45123e729d40bd21101ec
sjiampojamarn <18257803+sjiampojamarn@users.noreply.github.com>
2022-08-22 14:20:56 -0700

    Print out current certificate information
    Print out expiration online

--- register.sh
+++ register.sh
@@ -3,6 +3,12 @@
 ## run forever
 while true ; do
   date
+  certbot certificates
+  
+  for d in $(cat /conf/register.list | sed "s/,/ /g") ; do 
+    echo $d ;
+    echo "Q" | openssl s_client -servername $d -connect ${d}:443 2>/dev/null | openssl x509 -noout -dates ;
+  done
 
   while IFS= read -r line ; do  
     echo $line
