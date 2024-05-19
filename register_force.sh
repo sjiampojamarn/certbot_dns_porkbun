eb022781dd52d663a62b643d5c2ebba80f04ba76
sjiampojamarn <18257803+sjiampojamarn@users.noreply.github.com>
2024-05-19 09:03:52 -0700

    Create register_force.sh

---
+++ register_force.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+## init files 
+touch /conf/porkbun.ini
+touch /conf/email.ini
+touch /conf/register.list
+
+chmod 700 /conf/porkbun.ini
+
+## run once
+  date
+  certbot certificates
+  
+  for d in $(cat /conf/register.list | sed "s/,/ /g") ; do 
+    echo $d ;
+    echo "Q" | openssl s_client -servername $d -connect ${d}:443 2>/dev/null | openssl x509 -noout -dates ;
+  done
+
+  while IFS= read -r line ; do  
+    echo $line
+    set -x
+    certbot certonly --force-renewal \
+      --non-interactive \
+      --agree-tos \
+      --email $(cat /conf/email.ini) \
+      --preferred-challenges dns \
+      --authenticator dns-porkbun \
+      --dns-porkbun-credentials /conf/porkbun.ini \
+      --dns-porkbun-propagation-seconds 630 \
+      --expand \
+      -d "$line" 
+    set +x
+  done < "/conf/register.list"
+  
+  ## Post command to prepare pem for HAProxy.
+  ./combineFullPrivKeys.sh
+  
+  set -x
